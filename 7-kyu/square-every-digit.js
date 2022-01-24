6304b50d10ec1e4a34b99dd4d50ad164374840ac
lucas <lucasportellaagu@gmail.com>
2022-01-24 11:09:08 -0300

    square-every-digit

---
+++ 7-kyu/square-every-digit.js
@@ -0,0 +1,10 @@
+function squareDigits(num){
+    result = ''
+    for (let i = 0; i < String(num).length; i += 1) {
+        result += String(Number(String(num)[i]) * Number(String(num)[i]))
+    }
+    return Number(result);
+  }
+
+console.log(squareDigits(9119))
+// 811181
